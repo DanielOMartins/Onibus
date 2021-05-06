b4538b334f4fedd78ec77ad594dfb78dc0c96b67
DanielOMartins <78224425+DanielOMartins@users.noreply.github.com>
2021-05-06 17:21:07 -0300

    Update AppOnibus.java

--- AppOnibus.java
+++ AppOnibus.java
@@ -2,8 +2,7 @@ public class AppOnibus {
     public static void main(String[] args) {
         Onibus onibus = new Onibus();
 
-        onibus.iniciarOnibus();
-        
+        onibus.iniciarOnibus(); 
     }
 }
  
